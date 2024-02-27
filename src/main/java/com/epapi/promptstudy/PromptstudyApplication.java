7dd2d45408838df803ff8d970f14efb991305529
이경용 <sleekydz86@icloud.com>
2024-02-27 12:41:45 +0900

    PromptApi 베이직 셋팅

---
+++ src/main/java/com/epapi/promptstudy/PromptstudyApplication.java
@@ -0,0 +1,13 @@
+package com.epapi.promptstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromptstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epapi/promptstudy/PromptstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epapi.promptstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
